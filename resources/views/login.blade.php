d3de3740af40b280a5cf462ad51c1e805a3f8c3f
Jimmy <montenegro.jimmy@gmail.com>
2026-09-03 11:23:24 -0400

    Mejora autofill del login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,17 @@
     <title>Gestión - BICOM PICKERS</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
+    <style>
+        /* Neutraliza el fondo azul feo del autocompletado en Chrome */
+        input:-webkit-autofill,
+        input:-webkit-autofill:hover, 
+        input:-webkit-autofill:focus, 
+        input:-webkit-autofill:active {
+            -webkit-box-shadow: 0 0 0 30px white inset !important;
+            -webkit-text-fill-color: #0f172a !important; /* Color del texto oscuro (#111111 o slate-900) */
+            transition: background-color 5000s ease-in-out 0s;
+        }
+    </style>
 </head>
 <body class="bg-slate-100 min-h-screen flex items-center justify-center">
 
